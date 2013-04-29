064d9c44fd18b055463429406f14a100f89db6e2
MinoWu <minowu@foxmail.com>
2013-04-29 21:53:25 +0800

    使Api继承Restful Controller。

--- Controller/Api.php
+++ Controller/Api.php
@@ -1,6 +1,6 @@
 <?php namespace Smartadmin\Controller;
 
-use Think\Controller as Controller;
+use Smartadmin\Controller\Restful as Controller;
 use Think\Debug as Debug;
 use Think\Exception as Exception;
 use Think\Lang as Lang;
@@ -10,21 +10,6 @@ use Think\Lang as Lang;
  * 后台开发CURD及页面控制器
  */
 class Api extends Controller {
-
-	public function _empty()
-	{
-		$method = array('get', 'post', 'put', 'delete', 'head');
-		$current = strtolower($_SERVER['REQUEST_METHOD']);
-
-		if(in_array($current, $method))
-		{
-			$class = $current . '_' . ACTION_NAME;
-			$this->$class();
-		}
-		else {
-			Debug::output(new Exception("不存在'$method'方法"));
-		}
-	}
 
 	protected function errorJson(Exception $error)
 	{

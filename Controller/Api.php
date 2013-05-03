1f81e9e56c1d1c8a989e728ab705439a4d2c6e42
Mino Wu <minowu@foxmail.com>
2013-05-03 13:37:09 +0800

    为Api添加successJson。

--- Controller/Api.php
+++ Controller/Api.php
@@ -18,4 +18,28 @@ class Api extends Controller {
 		$this->assign('error_msg', Lang::get($error->getMessage()));
 		$this->json();
 	}
+
+	protected function successJson($output = null, $is_array = false, $merge = array())
+	{
+		// 成功
+		$this->assign('success', 1);
+
+		// 是否存在输出
+		if(!is_null($output))
+		{
+			// 输出是否为复数
+			if($is_array)
+			{
+				$this->assign('datas', $output);
+			}
+			else {
+				$this->assign('data', $output);
+			}
+		}
+
+		// 合并额外设置
+		$this->vars = array_merge($this->vars, $merge);
+
+		$this->json();
+	}
 }
